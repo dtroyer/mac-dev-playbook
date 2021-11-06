7a38531e3d1f045cb2d479192899018cc640576f
Dean Troyer <dtroyer@gmail.com>
2021-11-05 21:43:37 -0500

    Add lafarer.osx-defaults
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- more-defaults.sh
+++ more-defaults.sh
@@ -12,3 +12,16 @@ sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
 
 echo "Set Caps Lock to Ctrl"
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000e0}]}'
+
+echo "Trackpad: enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+echo "Always show scrollbars"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+echo "Finder Preview Pane defaults"
+for i in public.archive public.audio public.image public.item public.movie public.presentation public.text; do
+    defaults write com.apple.finder PreviewPaneSettings -dict-add "$i" '{ showQuickActions = 0; }'
+done
